aa623cb652cc4f7448ffb8855a81083de06a4c74
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2025-09-08 08:47:48 +0530

    Time: 1 ms (42.07%), Space: 44.1 MB (82%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.java
@@ -15,33 +15,49 @@
  */
 class Solution {
 
-    class Pair<U,V>{
-        U first ;
-        V second ; 
+    // class Pair<U,V>{
+    //     U first ;
+    //     V second ; 
 
-        Pair(U first , V second){
-            this.first = first;
-            this.second = second;
-        }
-    }
-    // first = sum , second = no of node
+    //     Pair(U first , V second){
+    //         this.first = first;
+    //         this.second = second;
+    //     }
+    // }
+    // // first = sum , second = no of node
 
-    int count=0;
+    // int count=0;
 
-    private Pair<Integer,Integer> dfs(TreeNode root){
-        if(root==null) return new Pair<>(0,0);
+    // private Pair<Integer,Integer> dfs(TreeNode root){
+    //     if(root==null) return new Pair<>(0,0);
 
-        Pair<Integer,Integer> left = dfs(root.left);
-        Pair<Integer,Integer> right = dfs(root.right);
+    //     Pair<Integer,Integer> left = dfs(root.left);
+    //     Pair<Integer,Integer> right = dfs(root.right);
 
-        int sum = left.first + right.first + root.val;
-        int no = left.second+ right.second + 1;
+    //     int sum = left.first + right.first + root.val;
+    //     int no = left.second+ right.second + 1;
+
+    //     if(Math.floor(sum/no)==root.val) count++;
+
+    //     return  new Pair<>(sum,no);
+    // }
+
+
+     int count=0;
+
+    private int[] dfs(TreeNode root){
+        if(root==null) return new int[]{0,0};
+
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+
+        int sum = left[0] + right[0] + root.val;
+        int no = left[1]+ right[1] + 1;
 
         if(Math.floor(sum/no)==root.val) count++;
 
-        return  new Pair<>(sum,no);
+        return  new int[]{sum,no};
     }
-
 
     public int averageOfSubtree(TreeNode root) {
         dfs(root);
