af9a52f57550e1d2984bcdefaf5f1cb36d006967
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2025-09-21 08:18:26 +0530

    Time: 0 ms (100%), Space: 44.2 MB (87.64%) - LeetHub

---
+++ 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    boolean isdeleted ;
+    // boolean isrootdeleted;
+    private boolean dfs(TreeNode root, int target){
+        if(root==null) return false;
+        if(root.val==target && root.left==null && root.right==null){
+            isdeleted=true;
+            return true;
+        }
+        boolean left = dfs(root.left,target);
+        boolean right = dfs(root.right,target);
+
+        if(left) root.left=null;
+        if(right) root.right=null;
+        if(root.val==target && root.left==null && root.right==null){
+            isdeleted=true;
+            return true;
+        }
+
+        return false;
+
+    }
+
+
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        isdeleted=true;
+        while(isdeleted){
+            isdeleted=false;
+           boolean rootdeleted= dfs( root, target);
+           if(rootdeleted) return null;
+        }
+        return root;
+    }
+}
