6bfbe995c6e23f09c4af63815b2654f63fb7e243
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2025-09-27 10:44:57 +0530

    Time: 1 ms (70.31%), Space: 48.5 MB (57.52%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
@@ -1,45 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public TreeNode reverseOddLevels(TreeNode root) {
-        Deque<TreeNode> q1 = new LinkedList<>();
-        Deque<TreeNode> q2 = new LinkedList<>();
 
-        q1.add(root);
-        int level = 0;
-        while (!q1.isEmpty()) {
-            int len = q1.size();
-            if (level % 2 == 0) {
-                for (int i = 0; i < len; i++) {
-                    TreeNode curr = q1.remove();
-                    if (curr.left != null) q1.add(curr.left);
-                    if (curr.right != null) q1.add(curr.right);
-                }
-            } else {
-                int i = 0, j = len - 1;
-                while (i < j) {
-                    TreeNode curr1 = q1.removeFirst();
-                    TreeNode curr2 = q1.removeLast();
-                    
-                    int temp = curr1.val;
-                    curr1.val = curr2.val;
-                    curr2.val = temp;
+    private void swap(TreeNode Left , TreeNode Right ,int level ){
+        if(Left == null || Right == null) return ;
 
-                    if (curr1.left != null) q2.add(curr1.left);
-                    if (curr1.right != null) q2.add(curr1.right);
-                    if (curr2.right != null) q2.offerFirst(curr2.right);
-                    if (curr2.left != null) q2.offerFirst(curr2.left);
-
-                    i++;
-                    j--;
-                }
-
-                while(!q2.isEmpty()){
-                    q1.add(q2.poll());
-                }
-                
-                
-            }
-            level++;
+        if(level%2==1){
+            int temp =Left.val;
+            Left.val = Right.val;
+            Right.val = temp;
         }
+
+        swap(Left.left,Right.right,level+1);
+        swap(Left.right,Right.left,level+1);
+    }
+
+    public TreeNode reverseOddLevels(TreeNode root) {
+        swap(root.left,root.right,1);
         return root;
     }
 }
