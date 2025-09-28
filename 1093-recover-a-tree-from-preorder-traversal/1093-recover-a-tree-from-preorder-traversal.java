e2892698545e7c1921f7f91f0501f77a2c1a2e39
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2025-09-28 09:34:26 +0530

    Time: 1 ms (100%), Space: 45 MB (32.04%) - LeetHub

---
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int idx;
+
+    private TreeNode dfs(String t , int level){
+        if(idx>=t.length()) return null;
+
+        int val = 0;
+while (idx < t.length() && Character.isDigit(t.charAt(idx))) {
+    val = val * 10 + (t.charAt(idx) - '0');
+    idx++;
+}
+        TreeNode root = new TreeNode(val);
+        int count=0;
+        int tempidx=idx;
+        while(tempidx<t.length() && t.charAt(tempidx)=='-') {
+            count++;
+            tempidx++;
+            }
+        if(count==level+1){
+            idx=tempidx;
+            root.left = dfs(t,level+1);
+        } 
+        // else return root;
+        count=0;
+        tempidx=idx;
+        while(tempidx<t.length() && t.charAt(tempidx)=='-') {
+            count++;
+            tempidx++;
+            }
+        if(count==level+1){
+            idx=tempidx;
+            root.right = dfs(t,level+1);
+        } 
+        // else return root;
+
+        return root;
+    }
+
+    public TreeNode recoverFromPreorder(String traversal) {
+        idx=0;
+        return dfs(traversal,0);
+    }
+}
