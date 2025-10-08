5a83afa927814ca58fb827895b8339af6fa5845c
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2025-10-08 10:38:27 +0530

    Time: 57 ms (27.53%), Space: 45.1 MB (81.6%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
@@ -1,51 +1,42 @@
 class Solution {
 
-     private boolean bfs(int effort ,int[][] height,boolean[][] visited ){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {0,0});
-        visited[0][0]=true;
+     private void dijkastra(int[][] height,int[][] result){
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> a[2] - b[2]);
+        q.offer(new int[] {0,0,0});
 
         int[][] direction = {{0,1},{1,0},{0,-1},{-1,0}};
 
-        while(!q.isEmpty()){
-            int[] curr = q.poll();
+        while(!q.isEmpty()){ 
+            int[] curr = q.poll(); 
             int row = curr[0];
             int col  =curr[1];
-            
+            int distance = curr[2];
 
-            if(row==height.length-1 && col==height[0].length-1) return true;
+            // if(row==height.length-1 && col==height[0].length-1) return true;
 
             for(int[] dir : direction){
                 int x= row+dir[0];
                 int y= col+dir[1];
 
-                if(x>=0 && x<height.length && y>=0 && y<height[0].length && !visited[x][y]){
-                    int diff = Math.abs(height[x][y] - height[row][col]);
-                    if(diff <= effort){
-                        q.offer(new int[] {x,y});
-                        visited[x][y]=true;
-                    } 
+                if(x>=0 && x<height.length && y>=0 && y<height[0].length ){
+                   int newEffort = Math.max(distance, Math.abs(height[x][y] - height[row][col]));
+                    if (newEffort < result[x][y]) {
+                        result[x][y] = newEffort;
+                        q.offer(new int[]{ x, y,newEffort});
+                    }
                 }
             }
-
-           
         }
-
-        return false;
     }
 
     public int minimumEffortPath(int[][] height) {
-        int s=0,e=999999,ans=0;
-        while(s<=e){
-            int mid = s+(e-s)/2;
-            boolean[][] visited = new boolean[height.length][height[0].length];
-            if(bfs(mid,height,visited)){
-                ans=mid;
-                e=mid-1;
-            }
-            else s= mid+1;
-        }
-        return ans;
+        int[][] result = new int[height.length][height[0].length];
+
+        for (int[] row : result) Arrays.fill(row, Integer.MAX_VALUE);
+        
+        result[0][0] = 0;
+        dijkastra(height,result);
+        return result[height.length-1][height[0].length-1];
     }
    
 }
@@ -59,5 +50,7 @@ now how to check paths ?
 we can move in 4 direction we will move to next path on when less or equal  to effort given 
 we will use bfs start 0,0 and when we will get n-1,m-1 we will return that true
 will mark each cell we visit by -1 to not return 
+
+T.C = logn
 
 */
