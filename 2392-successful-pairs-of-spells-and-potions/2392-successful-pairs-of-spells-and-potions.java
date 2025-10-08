99346cdeb1347a195b1cf58e28011a8d5a5a15fe
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2025-10-08 09:09:24 +0530

    Time: 50 ms (17.73%), Space: 66.9 MB (21.19%) - LeetHub

---
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n=spells.length;
+        int m= potions.length;
+
+        Arrays.sort(potions);
+
+        int[] ans = new int[n];
+        int i=0;
+        while(i<n){
+            ans[i]=binarysearch(0,m-1,potions,spells[i],success);
+            i++;
+        }
+        return ans;
+    }
+
+    private int binarysearch(int s,int e,int[] arr,int num,long target){
+        int ans=-1;
+        int end= e;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if((long)num*arr[mid]>=target){
+                ans=mid;
+                e=mid-1;
+            }
+            else s=mid+1;
+        }
+
+        return (ans==-1) ? 0: end-ans+1;  
+    }
+
+}
+
+/*
+    basically given 2 array want to find for each no of product for which product value  > success and return an arrray for each value in spells 
+
+    if we see this as if we sort both nlogn + mlogm do binary search for all n elemnt will get result in logn 
+
+    t.c =  mlogm + n*logm  = m
+
+
+    thinking of applying binary search as if sorted then we just have to find starting potion position which allow us to make succes then all next will be done 
+*/ 
