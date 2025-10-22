6030095824ee4e7feea4bdf29b4af6418d30096e
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2025-10-22 17:57:56 +0530

    Time: 6 ms (81.81%), Space: 44.3 MB (72.75%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -1,28 +1,39 @@
 class Solution {
+        
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         List<List<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        for (int[] flight : flights) {
-            adj.get(flight[0]).add(new int[] {flight[1], flight[2]});
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        for(int i=0;i<flights.length;i++){
+            adj.get(flights[i][0]).add(new int[] {flights[i][1],flights[i][2]});
         }
+        int step=0;
+
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {src, 0});
-        int[] minCost = new int[n];
-        Arrays.fill(minCost, Integer.MAX_VALUE);
-        int stops = 0;
-        while (!q.isEmpty() && stops <= k) {
-            int size = q.size();
-            while (size-- > 0) {
-                int[] curr = q.poll();
-                for (int[] neighbour : adj.get(curr[0])) {
-                    int price = neighbour[1], neighbourNode = neighbour[0];
-                    if (price + curr[1] >= minCost[neighbourNode]) continue;
-                    minCost[neighbourNode] = price + curr[1];
-                    q.offer(new int[] {neighbourNode, minCost[neighbourNode]});
-                }
+        q.offer(new int[] {src,0});
+
+        int[] cost = new int[n];
+        Arrays.fill(cost,Integer.MAX_VALUE);
+        cost[src] = 0; 
+        
+        while(!q.isEmpty() && step<=k){
+          int len = q.size();
+          int[] temp = Arrays.copyOf(cost, n);
+          for(int i=0;i<len;i++){
+            int[] curr = q.poll();
+            int u= curr[0] , price = curr[1];
+        
+            for(int[] v : adj.get(u)){
+                 if (price+v[1] < temp[v[0]]) { 
+                        temp[v[0]] = price+v[1];
+                        q.offer(new int[]{v[0], price+v[1]});
+                    }
             }
-            stops++;
+          }
+          cost=temp;
+          step++;
         }
-        return minCost[dst] == Integer.MAX_VALUE ? -1 : minCost[dst];
+
+        return (cost[dst]==Integer.MAX_VALUE) ? -1 :cost[dst];  
+      
     }
 }
