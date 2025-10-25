4df2425186a54d8ba75139640fbcfa4afdcb377c
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2025-10-25 15:56:51 +0530

    Time: 23 ms (82.47%), Space: 68.5 MB (63.47%) - LeetHub

--- 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.java
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.java
@@ -1,32 +1,27 @@
 class Solution {
-    int max = -1;
-
+    int max =-1;
     public int longestCycle(int[] edges) {
-        int n = edges.length;
-        boolean[] visited = new boolean[n];
+        int n= edges.length;
+        boolean[] visited =  new boolean[n];
         int[] inrecursion = new int[n];
-        Arrays.fill(inrecursion, -1);
-        for (int i = 0; i < n; i++) {
-            if (!visited[i])
-                dfs(i, edges, visited, inrecursion, 0);
+        Arrays.fill(inrecursion,-1);
+        for(int i=0;i<n;i++){
+            if(!visited[i]) dfs(i,edges,visited, inrecursion,0);
         }
         return max;
     }
 
-    private void dfs(int u, int[] edges, boolean[] visited, int[] inrecursion, int count) {
-        if (visited[u]) return;
-
-        visited[u] = true;
-        inrecursion[u] = count;
-        count++;
-
-        if (edges[u] != -1) {
-            if (inrecursion[edges[u]] != -1)
-                max = Math.max(max, count - inrecursion[edges[u]]);
-            else
-                dfs(edges[u], edges, visited, inrecursion, count);
+    private void dfs(int u , int[] edges ,boolean[] visited , int[] inrecursion,int count ){
+        if(visited[u] && inrecursion[u]!=-1){
+            max = Math.max(max,count-inrecursion[u]);
+            return;
         }
-        inrecursion[u] = -1;
+        if(visited[u]) return;
+        visited[u]=true;
+        inrecursion[u]=count;
+        count++;
+        if(edges[u]!=-1) dfs(edges[u],edges,visited,inrecursion,count);
+        inrecursion[u]=-1;
     }
 }
 
