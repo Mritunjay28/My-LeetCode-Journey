6846d6bb184fca48e738f8f8d963a7146ad34d52
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2025-12-09 08:20:54 +0530

    Time: 500 ms (5.43%), Space: 280.7 MB (5.43%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int specialTriplets(int[] nums) {
+        int[] right = new int[1000000];
+        for(int i=0;i<nums.length;i++) right[nums[i]]++;
+
+        long ans=0;
+        int mod= 1000000007;
+
+        int[] left = new int[1000000];
+        for(int i=0;i<nums.length;i++){
+            right[nums[i]]--;
+            int target = nums[i]*2;
+            if(target <100001) ans= (ans+1L * left[target]*right[target])%mod;
+            left[nums[i]]++;
+        }
+        return (int)ans;
+    }
+}
