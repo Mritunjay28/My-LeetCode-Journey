49c3bc8c8df0383c7326810517710de230eeeb7f
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2025-12-13 07:52:33 +0530

    Time: 30 ms (15%), Space: 47.9 MB (26.36%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.java
+++ 3606-coupon-code-validator/3606-coupon-code-validator.java
@@ -1,31 +1,31 @@
 class Solution {
     public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
-        Map<String,Integer> businessorder =new HashMap<>();
-        businessorder.put("electronics",0);
-        businessorder.put("grocery",1);
-        businessorder.put("pharmacy",2);
-        businessorder.put("restaurant",3);
+        List<String[]> valid= new ArrayList<>();
 
-        List<String[]> valid = new ArrayList<>();
-        for(int i=0 ; i<code.length;i++){
+        HashMap<String,Integer> priority = new HashMap<>();
+        priority.put("electronics",0);
+        priority.put("grocery",1);
+        priority.put("pharmacy",2);
+        priority.put("restaurant",3);
+
+
+        for(int i=0;i<code.length;i++){
             String c = code[i];
-            String b = businessLine[i];
-            boolean active = isActive[i];
-
-            if(!c.isEmpty() && c.matches("[a-zA-Z0-9_]+") && businessorder.containsKey(b) && active){
+            String b= businessLine[i];
+            boolean active = isActive[i] ;
+            if(c!=null && c.matches("^[A-Za-z0-9_]+$") && priority.containsKey(b) && active) {
                 valid.add(new String[]{c,b});
             }
         }
 
-        valid.sort((a,b) -> {
-           int compare = Integer.compare(businessorder.get(a[1]),businessorder.get(b[1])) ;
+        valid.sort((a,b)->{
+            int compare = Integer.compare(priority.get(a[1]),priority.get(b[1]));
             if(compare!=0) return compare;
             return a[0].compareTo(b[0]);
         });
 
-        List<String> ans = new ArrayList<>();
-        for(String[] arr :valid ) ans.add(arr[0]);
-
+        List<String> ans= new ArrayList<>();
+        for(String[] arr : valid) ans.add(arr[0]);
         return ans;
     }
 }
