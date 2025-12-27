2a3fe7e7a1192574178b48b20ad23cf15a29284f
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2025-12-27 09:42:00 +0530

    Time: 104 ms (60.62%), Space: 141.8 MB (51%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,26 +1,25 @@
 class NumMatrix {
 
-    int[][] arr ;
+    int[][] arr;
     public NumMatrix(int[][] matrix) {
-        arr= new int[matrix.length][matrix[0].length];
+        arr = new int[matrix.length][matrix[0].length];
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-               if(i==0 && j==0) arr[i][j] = matrix[i][j];
-               else if(i==0 && j!=0) arr[i][j] = arr[i][j-1] + matrix[i][j];
-               else if(i!=0 && j==0) arr[i][j] = arr[i-1][j] + matrix[i][j];
-               else{
-                arr[i][j]= matrix[i][j]+arr[i-1][j] + arr[i][j-1]-arr[i-1][j-1] ;
-               } 
+            for(int j=0;j<matrix[0].length;j++){
+                if(i==0 && j==0) arr[i][j] = matrix[i][j];
+                else if(i!=0 && j==0) arr[i][j] = matrix[i][j] + arr[i-1][j];
+                else if(i==0 && j!=0) arr[i][j] = matrix[i][j] + arr[i][j-1];
+                else arr[i][j] = matrix[i][j] + arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-       int a =0,b=0,c=0;
-       if(row1-1>=0) a= arr[row1-1][col2];
-       if(col1-1>=0) b= arr[row2][col1-1];
-       if(row1-1>=0 && col1-1>=0) c= arr[row1-1][col1-1];
-       return arr[row2][col2] -a-b+c;
+        int n = arr.length , m= arr[0].length;
+        int a =0,b=0,c=0;
+        if(col1>0) a=arr[row2][col1-1];
+        if(row1>0) b =arr[row1-1][col2];
+        if(row1>0 && col1>0) c =arr[row1-1][col1-1];
+        return arr[row2][col2] +c - a-b ;
     }
 }
 
