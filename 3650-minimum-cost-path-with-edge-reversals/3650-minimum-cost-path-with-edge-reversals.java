1f1effd43facdb92005c6e16d6c5d07f6f99d319
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-01-27 08:10:41 +0530

    Time: 69 ms (93.93%), Space: 205.9 MB (83.8%) - LeetHub

---
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minCost(int n, int[][] edges) {
+        List<List<int[]>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        for(int i=0;i<edges.length;i++){
+            adj.get(edges[i][0]).add(new int[] {edges[i][1],edges[i][2]}); // v,cost
+            adj.get(edges[i][1]).add(new int[] {edges[i][0],2*edges[i][2]});
+        }
+        boolean[] visited = new boolean[n];
+
+        int[] dist = new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[0]=0;
+
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->a[1]-b[1]);
+        q.add(new int[] {0,0}); // u,cost 
+
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int u= curr[0] , currweight =curr[1];
+            if(currweight>dist[u]) continue;
+            if(u==n-1) return currweight;
+
+            for(int[] next : adj.get(u)){
+                if(next[1]+currweight <dist[next[0]]){
+                    dist[next[0]] = next[1]+currweight;
+                    q.add(new int[] {next[0],next[1]+currweight});
+                }
+            }
+        }
+
+        return (dist[n-1]==Integer.MAX_VALUE) ? -1 : dist[n-1];
+    }
+}
+// dijkastra 
