2ebb1c27baa7eca4499288df9d84af3d04c6a839
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-03-30 07:48:31 +0530

    Time: 725 ms (50%), Space: 281.4 MB (50%) - LeetHub

--- 3882-minimum-xor-path-in-a-grid/3882-minimum-xor-path-in-a-grid.java
+++ 3882-minimum-xor-path-in-a-grid/3882-minimum-xor-path-in-a-grid.java
@@ -1,53 +1,39 @@
+
 class Solution {
     public int minCost(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        
-        // Since m * n <= 1000, we can map (i, j) to a single index: i * n + j
-        // dp[cell_index][xor_value] = true if xor_value is reachable at cell_index
-        boolean[][] dp = new boolean[m * n][1024];
 
-        // Base case: Starting point (0, 0)
-        dp[0][grid[0][0]] = true;
-
+        // dp[i][j] will store all possible XOR values to reach cell (i, j)
+        HashSet<Integer>[][] dp = new HashSet[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int currIdx = i * n + j;
-                int val = grid[i][j];
+                dp[i][j] = new HashSet<>();
+            }
+        }
 
-                // If we are not at the start, we check neighbors
-                if (i == 0 && j == 0) continue;
+        dp[0][0].add(grid[0][0]);
 
-                // Check Top neighbor (i-1, j)
+        for (int i = 0; i < m; i++) {
+            for (int j = (i == 0 ? 1 : 0); j < n; j++) {
                 if (i > 0) {
-                    int topIdx = (i - 1) * n + j;
-                    for (int k = 0; k < 1024; k++) {
-                        if (dp[topIdx][k]) {
-                            dp[currIdx][k ^ val] = true;
-                        }
+                    for (int x : dp[i - 1][j]) {
+                        dp[i][j].add(x ^ grid[i][j]);
                     }
                 }
-
-                // Check Left neighbor (i, j-1)
                 if (j > 0) {
-                    int leftIdx = i * n + (j - 1);
-                    for (int k = 0; k < 1024; k++) {
-                        if (dp[leftIdx][k]) {
-                            dp[currIdx][k ^ val] = true;
-                        }
+                    for (int x : dp[i][j - 1]) {
+                        dp[i][j].add(x ^ grid[i][j]);
                     }
                 }
             }
         }
 
-        // Find the minimum XOR value reachable at the last cell (m-1, n-1)
-        int lastIdx = (m * n) - 1;
-        for (int k = 0; k < 1024; k++) {
-            if (dp[lastIdx][k]) {
-                return k;
-            }
+        // Find minimum XOR value in the last cell
+        int min = Integer.MAX_VALUE;
+        for (int val : dp[m - 1][n - 1]) {
+            min = Math.min(min, val);
         }
-
-        return -1; 
+        return min;
     }
 }
