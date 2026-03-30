a7592aecd53a6da5ccb582375c61c6535b0ade8c
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-03-30 07:10:43 +0530

    Time: 7 ms (25%), Space: 48.2 MB (22.37%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,24 +1,22 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
         int n = s1.length();
-        HashMap<Character,Integer> even = new HashMap<>();
-        HashMap<Character,Integer> odd = new HashMap<>();
+        int[] even = new int[26];
+        int[] odd = new int[26];
 
         for(int i=0;i<n;i++) {
-            if(i%2==0) even.put(s1.charAt(i),even.getOrDefault(s1.charAt(i),0)+1);
-            else odd.put(s1.charAt(i),odd.getOrDefault(s1.charAt(i),0)+1);
+            if(i%2==0) even[s1.charAt(i)-'a']++;
+            else odd[s1.charAt(i)-'a']++;
         }
 
         for(int i=0;i<n;i++) {
             if(i%2==0){
-                if(!even.containsKey(s2.charAt(i))) return false;
-                even.put(s2.charAt(i),even.get(s2.charAt(i))-1);
-                if(even.get(s2.charAt(i))==0) even.remove(s2.charAt(i));
+                if(even[s2.charAt(i)-'a']==0) return false;
+                even[s2.charAt(i)-'a']--;
             }
             else {
-                 if(!odd.containsKey(s2.charAt(i))) return false;
-                odd.put(s2.charAt(i),odd.get(s2.charAt(i))-1);
-                if(odd.get(s2.charAt(i))==0) odd.remove(s2.charAt(i));
+                if(odd[s2.charAt(i)-'a']==0) return false;
+                odd[s2.charAt(i)-'a']--;
             }
         }
 
