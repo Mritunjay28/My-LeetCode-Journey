045647cfc92360db9d50330b9cbe3769bcdeaee9
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-04-06 07:57:40 +0530

    Time: 34 ms (59.78%), Space: 51.6 MB (80.07%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.java
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.java
@@ -1,114 +1,35 @@
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
-        int res =0;
-        int x =0,y=0;
-        int dir=0;
-        for(int i=0;i<commands.length;i++){
-            if(commands[i]==-1){
-                dir++;
-                dir = dir%4;
-            }
-            else if (commands[i]==-2){
-                dir--;
-                if(dir<0) {
-                    dir = (dir+4)%4;
-                }
-            }
-            else{
-                if(dir==1){
-                    // check if same y exist 
-                    boolean update = false;
-                    int gap=-1;
-                    int ans=-1;
-                    for(int j =0;j<obstacles.length;j++){
-                        if(obstacles[j][1]==y && obstacles[j][0]>x && x+commands[i] >=obstacles[j][0]) {
-                            if(gap==-1){
-                                 gap= obstacles[j][0]-x;
-                             update=true;
-                             ans=obstacles[j][0];
-                            }
-                            if(obstacles[j][0]-x<gap){
-                                 update=true;
-                             ans=obstacles[j][0];
-                              gap= obstacles[j][0]-x;
-                            }
-                        }
-                    }
-                    if(!update) x +=commands[i];
-                    else x=ans-1;
-                }
-                else if (dir==3){
-                    // check if same y exist 
-                    boolean update = false;
-                     int gap=Integer.MAX_VALUE;
-                    int ans=1;
-                    for(int j =0;j<obstacles.length;j++){
-                        if(obstacles[j][1]==y && obstacles[j][0]<x && x-commands[i] <=obstacles[j][0]) {
-                            if(gap==1){
-                                gap= x-obstacles[j][0];
-                                update=true;
-                            ans=obstacles[j][0];
-                            }
-                            if(x-obstacles[j][0] <gap) {
-                                 update=true;
-                             ans=obstacles[j][0];
-                              gap= x-obstacles[j][0];
-                            }
-                            
-                        }
-                    }
-                    if(!update) x -=commands[i];
-                    else x=ans+1;
-                }
-                else if(dir==0){
-                    // check same x 
-                     boolean update = false;
-                    int gap=-1;
-                    int ans=-1;
-                    for(int j =0;j<obstacles.length;j++){
-                        if(obstacles[j][0]==x && obstacles[j][1]>y && y+commands[i] >=obstacles[j][1] ) {
-                            if(gap==-1){
-                                 gap= obstacles[j][1]-y;
-                             update=true;
-                             ans=obstacles[j][1];
-                            }
-                            if(obstacles[j][1]-y<gap){
-                                 update=true;
-                             ans=obstacles[j][1];
-                              gap= obstacles[j][1]-y;
-                            }
-                        }
-                    }
-                    if(!update) y +=commands[i];
-                    else y=ans-1;
-                }
-                else{
-                     boolean update = false;
-                     int gap=Integer.MAX_VALUE;
-                    int ans=1;
-                    for(int j =0;j<obstacles.length;j++){
-                        if(obstacles[j][0]==x && obstacles[j][1]<y && y-commands[i] <=obstacles[j][1]) {
-                            if(gap==1){
-                                gap= y-obstacles[j][1];
-                                update=true;
-                            ans=obstacles[j][1];
-                            }
-                            if(y-obstacles[j][1] <gap) {
-                                 update=true;
-                             ans=obstacles[j][1];
-                              gap= y-obstacles[j][1];
-                            }
-                            
-                        }
-                    }
-                    if(!update) y -=commands[i];
-                    else y=ans+1;
-                }
-
-                res= Math.max(res,x*x+y*y);
-            }
+      HashSet<String> set = new HashSet<>();
+      for(int[] obs: obstacles){
+        set.add(obs[0]+","+obs[1]);
+      }
+      int x=0;
+      int y=0;
+      int maxx=0;
+      int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
+      int d=0;
+      for(int command:commands) {
+        if(command==-1){
+            d++;
+            d=d%4;
         }
+        else if(command==-2){
+            d--;
+            d=(d+4)%4;
+        }
+        else{
+            for(int i=0;i<command;i++){
+                int xx=x+dir[d][0];
+                int yy=y+dir[d][1];
+                if(set.contains(xx+","+yy)) break;
+                x=xx;
+                y=yy;
+            }
 
-        return res;
+        }
+        maxx=Math.max(maxx,x*x+y*y);
+      }
+      return maxx;
     }
 }
