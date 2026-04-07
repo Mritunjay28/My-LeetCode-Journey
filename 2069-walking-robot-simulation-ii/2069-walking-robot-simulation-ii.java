f5e29a97f8f844f5e1bbf82684bba4c3eed41eb1
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-04-07 07:11:30 +0530

    Time: 2790 ms (5.59%), Space: 58.2 MB (13.29%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
@@ -0,0 +1,86 @@
+class Robot {
+    int minx = 0, miny = 0;
+    int maxx;
+    int maxy;
+    int x = 0, y = 0;
+    int dir = 0; // east
+
+    public Robot(int width, int height) {
+        maxx = width - 1;
+        maxy = height - 1;
+
+    }
+
+    public void step(int num) {
+        while (num > 0) {
+            if (dir == 0) {
+                if (x + num <= maxx){
+                    x += num;
+                    num=0;
+                } 
+                else {
+                    int diff = x + num - maxx;
+                    x = maxx;
+                    dir++;
+                    num = diff;
+                }
+            }
+            else if (dir == 1) {
+                if (y + num <= maxy){
+                    y += num;
+                    num=0;
+                }
+                else {
+                    int diff = y + num - maxy;
+                    y = maxy;
+                    dir++;
+                    num = diff;
+                }
+            }
+            else if (dir == 2) {
+                if (x - num >=0){
+                    x -= num;
+                    num=0;
+                } 
+                else {
+                    int diff = num-x;
+                    x = 0;
+                    dir++;
+                    num = diff;
+                }
+            }
+            else{
+                 if (y - num >=0){
+                    y -= num;
+                    num=0;
+                }
+                else {
+                    int diff = num-y;
+                    y = 0;
+                    dir=0;
+                    num = diff;
+                }
+            }
+
+        }
+    }
+
+    public int[] getPos() {
+        return new int[] {x,y};
+    }
+
+    public String getDir() {
+        if(dir==0) return "East";
+        else if(dir==1) return "North";
+        else if(dir==2) return "West";
+        else return "South";
+    }
+}
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot obj = new Robot(width, height);
+ * obj.step(num);
+ * int[] param_2 = obj.getPos();
+ * String param_3 = obj.getDir();
+ */
