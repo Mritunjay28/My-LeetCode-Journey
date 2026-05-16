768d892a421cd91fac57eb1a72d1489e764fd13a
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-05-16 09:12:09 +0530

    Time: 22 ms (5.69%), Space: 49 MB (62.57%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,38 +1,45 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
         Queue<int[]> q = new LinkedList<>();
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    q.add(new int[]{i, j});
-                } else {
-                    mat[i][j] = -1; // Mark 1s as unvisited
+        int[][] dis = new int[mat.length][mat[0].length];
+        boolean flag = true;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j]==0) {
+                    q.offer(new int[] {i,j});
+                    dis[i][j]=0;
                 }
-            }
-        }
-
-        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-        int level = 1;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int[] curr = q.poll();
-                for (int[] d : dirs) {
-                    int r = curr[0] + d[0];
-                    int c = curr[1] + d[1];
-                    if (r >= 0 && r < m && c >= 0 && c < n && mat[r][c] == -1) {
-                        mat[r][c] = level; 
-                        q.add(new int[]{r, c});
+                else {
+                    dis[i][j]=-1;
+                    flag=false;
                     }
-                }
             }
-            level++;
         }
 
-        return mat;
+        if(flag) return dis;
+
+        int l = 1;
+        // now do bfs for queue 
+        while(!q.isEmpty()){
+            int size= q.size();
+            for(int i=0;i<size;i++){
+                int[] temp = q.poll();
+
+                int[][] dir = {{0,1},{1,0},{-1,0},{0,-1}};
+
+                for(int[] d : dir){
+                    int x = temp[0]+d[0] , y=temp[1]+d[1];
+
+                    if(x>=0 && x<mat.length && y>=0 && y<mat[0].length && dis[x][y]==-1){
+                        dis[x][y]=l;
+                        q.offer(new int[] {x,y});
+                    } 
+                }
+            }
+            l++;
+        }
+
+        return dis;
+        
     }
 }
