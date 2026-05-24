0e56fe2a21d312147a3253dc75c4e80cb53bf8f4
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-05-24 11:04:37 +0530

    Time: 10 ms (87.43%), Space: 46.5 MB (41.71%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.java
+++ 1340-jump-game-v/1340-jump-game-v.java
@@ -1,72 +1,34 @@
 class Solution {
-    int max = 0;
-    int[][] memo = new int[1001][1001];
+    int[] memo = new int[1001];
 
     public int maxJumps(int[] arr, int d) {
-        // int prefix 
-        Stack<Integer> st = new Stack<>();
-        st.push(0);
-        int[] prefix = new int[arr.length];
-
-        for (int i = 1; i < arr.length; i++) {
-            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
-                st.pop();
-            }
-            if (st.isEmpty())
-                prefix[i] = i;
-            else
-                prefix[i] = i - st.peek() - 1;
-            st.push(i);
-        }
-
-         st = new Stack<>();
-        int[] suffix = new int[arr.length];
-
-        st.push(arr.length - 1);
-
-        for (int i = arr.length - 2; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
-                st.pop();
-            }
-
-            if (st.isEmpty()) {
-                suffix[i] = arr.length - 1 - i;
-            } else {
-                suffix[i] = st.peek() - i - 1;
-            }
-
-            st.push(i);
-        }
-
-        
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
-        }
+        int ans = 1;
 
         for (int i = 0; i < arr.length; i++) {
-            dfs(i, arr, d, 1,prefix,suffix);
-            if(max==arr.length) break;
+            ans = Math.max(ans, dfs(i, arr, d));
         }
-        return max;
+
+        return ans;
     }
 
-    public void dfs(int idx, int[] arr, int d, int count,int[] prefix,int[] suffix) {
+    public int dfs(int idx, int[] arr, int d) {
+        if (memo[idx] != 0) return memo[idx];
 
-        if(memo[idx][count]!=-1) return ;
-        
-        // check both side if possible to go
-        // left side - prefix 
-        for(int i=idx-1;i>=Math.max(idx-d,idx-prefix[idx]);i--){
-            dfs(i,arr,d,count+1,prefix,suffix);
+        int best = 1;
+
+        // left
+        for (int i = idx - 1; i >= Math.max(0, idx - d); i--) {
+            if (arr[i] >= arr[idx]) break;
+            best = Math.max(best, 1 + dfs(i, arr, d));
         }
 
-        // right side - suffix 
-        for(int i=idx+1;i<=Math.min(idx+d,idx+suffix[idx]);i++){
-          dfs(i,arr,d,count+1,prefix,suffix);
+        // right
+        for (int i = idx + 1; i <= Math.min(arr.length - 1, idx + d); i++) {
+            if (arr[i] >= arr[idx]) break;
+            best = Math.max(best, 1 + dfs(i, arr, d));
         }
 
-
-        max=Math.max(max,count);
-        memo[idx][count]=max;
+        memo[idx] = best;
+        return best;
     }
 }
