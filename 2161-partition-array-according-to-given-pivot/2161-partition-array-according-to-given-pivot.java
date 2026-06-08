ad0d0f71ae1dc7c6a5fa115628c17e8f69ee394f
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-06-08 08:22:14 +0530

    Time: 15 ms (15.65%), Space: 170.7 MB (5.16%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,17 +1,20 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
+        ArrayList<Integer> small = new ArrayList<>();
+        ArrayList<Integer> equal = new ArrayList<>();
+        ArrayList<Integer> large = new ArrayList<>();
         int[] ans = new int[nums.length];
-        int idx=0;
+    
         for(int i=0;i<nums.length;i++) {
-            if(nums[i]<pivot) ans[idx++] = nums[i];
+            if(nums[i]<pivot) small.add(nums[i]);
+            else  if(nums[i]==pivot) equal.add(nums[i]);
+            else   large.add(nums[i]);
         }
 
         for(int i=0;i<nums.length;i++) {
-            if(nums[i]==pivot) ans[idx++] = nums[i];
-        }
-
-        for(int i=0;i<nums.length;i++) {
-            if(nums[i]>pivot) ans[idx++] = nums[i];
+            if(i<small.size()) ans[i]=small.get(i);
+            else if ( i<small.size()+equal.size()) ans[i]=equal.get(i-small.size());
+            else ans[i]=large.get(i-small.size()-equal.size());
         }
 
         return ans;
