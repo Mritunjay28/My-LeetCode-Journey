62847eccf0eea9d2967592ee9d9ddfaba0c411cc
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-06-10 13:04:28 +0530

    Time: 1049 ms (5.2%), Space: 169.6 MB (67.34%) - LeetHub

--- 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.java
+++ 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.java
@@ -28,11 +28,12 @@ class Solution {
 
     public int bs(int l,int r,int a ,int b,int[] nums){
         int ans =-1;
+         int start = l;
 
         while(l<=r){
             int mid = l+(r-l)/2;
 
-            int max = check(l,mid,0,0,n-1, nums);
+            int max = check(start,mid,0,0,n-1, nums);
 
             if(max!=-1 && nums[max]>nums[a] && nums[max]>nums[b]){
                 ans=mid;
