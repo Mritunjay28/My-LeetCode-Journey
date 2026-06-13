bd973769ca8ca690bfb51cc92b470ae490a9f6f9
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-06-13 10:47:13 +0530

    Time: 560 ms (30.1%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-Select p.project_id,Round(Sum(e.experience_years )/Count(e.employee_id),2) as average_years 
+Select p.project_id,Round(AVG(e.experience_years ),2) as average_years 
 From Project p
 Left Join Employee e 
 On p.employee_id = e.employee_id
