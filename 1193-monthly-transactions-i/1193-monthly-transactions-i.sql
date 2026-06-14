9c345982e3005f05accf318cdaaaf4ca43263fa3
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-06-14 09:32:22 +0530

    Time: 574 ms (81.68%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -2,8 +2,8 @@
 SELECT DATE_FORMAT(trans_date , '%Y-%m') AS month,
        country,
        count(id) as trans_count,
-       count( case when state='approved ' then 1 end) as approved_count,
+       count( case when state='approved' then 1 end) as approved_count,
        Sum(amount ) as trans_total_amount,
-       Sum(case when state='approved ' then amount else 0 end ) as approved_total_amount 
+       Sum(case when state='approved' then amount else 0 end ) as approved_total_amount 
 FROM Transactions 
 GROUP BY DATE_FORMAT(trans_date, '%Y-%m') , country;
