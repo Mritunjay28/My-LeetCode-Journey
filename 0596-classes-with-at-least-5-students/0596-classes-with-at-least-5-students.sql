d8a2cd24637cc9b6bed2f9d13c91aa156df88a50
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-06-17 11:26:48 +0530

    Time: 353 ms (31.26%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,10 +1,8 @@
 # Write your MySQL query statement below
-Select Distinct c.class 
-From Courses c
-Join (
-    select class , count(student) as no_of_student
+
+select class 
 from Courses 
 group by class 
-) t
-On c.class = t.class And t.no_of_student >=5;
+having count(class) >=5;
+
 
