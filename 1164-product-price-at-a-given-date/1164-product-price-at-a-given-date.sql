956f6dda4bc4f8f0de263cdac1ff16a573c4732c
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-06-20 17:26:45 +0530

    Time: 477 ms (70.66%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -2,27 +2,22 @@
 
 SELECT p.product_id , p.new_price as price 
 FROM Products p
-JOIN (
+WHERE ( product_id , change_date) IN (
     SELECT product_id , MAX(change_date) as change_date
     FROM Products
     WHERE change_date <= '2019-08-16'
     GROUP BY product_id
-) t
-ON p.product_id = t.product_id 
-    AND p.change_date =t.change_date 
+)
 
 UNION 
 
 SELECT  p.product_id , 10 as price 
 FROM Products p
-LEFT JOIN (
-        SELECT product_id , MAX(change_date) as change_date
-        FROM Products
-        WHERE change_date <= '2019-08-16'
-        GROUP BY product_id
-) t
-ON p.product_id = t.product_id
-WHERE t.product_id IS NULL
+WHERE product_id NOT IN (
+    SELECT product_id
+    FROM Products
+    WHERE change_date<="2019-08-16"
+)
 
 
 
