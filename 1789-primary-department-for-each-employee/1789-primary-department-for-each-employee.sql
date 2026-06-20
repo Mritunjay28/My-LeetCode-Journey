15828690a2491d0be53828ea53d038d317eb4e0d
Mritunjay28 <mritunjaysenspati.dev@gmail.com>
2026-06-20 11:42:17 +0530

    Time: 569 ms (56.02%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -6,10 +6,6 @@ Where primary_flag = 'Y'
 Union 
 
 Select employee_id , department_id 
-From Employee 
-Where employee_id IN (
-    Select employee_id 
-    From Employee
-    Group By employee_id
-    Having Count(employee_id )=1
-)
+From Employee
+Group By employee_id
+Having Count(employee_id )=1
