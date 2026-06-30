b15d291d453bf9bf849478d60cab098c7a7ca770
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-06-30 15:38:37 +0530

    Time: 292 ms (99.08%), Space: 0B (100%) - LeetHub

---
+++ 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT user_id , ROUND(AVG(CASE
+                WHEN activity_type = 'free_trial' Then activity_duration 
+                END),2) AS trial_avg_duration 
+               , ROUND(AVG(CASE
+                WHEN activity_type = 'paid' Then activity_duration 
+                END),2) AS paid_avg_duration 
+FROM  UserActivity
+WHERE user_id IN (
+    SELECT DISTINCT user_id 
+    FROM UserActivity
+    WHERE activity_type = 'paid'
+)
+GROUP BY user_id 
+ORDER BY user_id 
