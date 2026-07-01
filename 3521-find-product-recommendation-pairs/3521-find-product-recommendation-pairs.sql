c1c7e0c26ab6a5d77457d31319e1625ba2a0d47b
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-07-01 10:16:09 +0530

    Time: 588 ms (91.19%), Space: 0B (100%) - LeetHub

--- 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -4,13 +4,9 @@ SELECT p1.product_id AS product1_id ,
        p4.category AS product2_category  ,
        COUNT(*)  AS customer_count 
 FROM ProductPurchases p1
-JOIN ProductPurchases p2
-ON p1.user_id =p2.user_id 
-AND p1.product_id < p2.product_id 
-LEFT JOIN ProductInfo p3
-ON p3.product_id = p1.product_id
-LEFT JOIN ProductInfo p4
-ON p4.product_id = p2.product_id
+JOIN ProductPurchases p2 ON p1.user_id =p2.user_id AND p1.product_id < p2.product_id 
+JOIN ProductInfo p3 ON p3.product_id = p1.product_id
+JOIN ProductInfo p4 ON p4.product_id = p2.product_id
 GROUP BY p1.product_id ,p2.product_id 
 HAVING COUNT(*) >=3
 ORDER BY customer_count DESC , product1_id , product2_id
