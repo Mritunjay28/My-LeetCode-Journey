26ec7fdb9aa4b5cb22f44429fc63407df0bb093c
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-07-01 10:14:15 +0530

    Time: 596 ms (87.48%), Space: 0B (100%) - LeetHub

---
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -0,0 +1,16 @@
+SELECT p1.product_id AS product1_id ,
+       p2.product_id AS product2_id ,
+       p3.category AS product1_category ,
+       p4.category AS product2_category  ,
+       COUNT(*)  AS customer_count 
+FROM ProductPurchases p1
+JOIN ProductPurchases p2
+ON p1.user_id =p2.user_id 
+AND p1.product_id < p2.product_id 
+LEFT JOIN ProductInfo p3
+ON p3.product_id = p1.product_id
+LEFT JOIN ProductInfo p4
+ON p4.product_id = p2.product_id
+GROUP BY p1.product_id ,p2.product_id 
+HAVING COUNT(*) >=3
+ORDER BY COUNT(*) DESC , p1.product_id , p2.product_id
