46e85419f6f7a58db80be7f3cc814467ca6e0fc7
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-07-01 14:01:14 +0530

    Time: 29 ms (52.66%), Space: 109.8 MB (42.44%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -0,0 +1,29 @@
+class Solution {
+    int max =-1;
+    public int longestCycle(int[] edges) {
+        int n= edges.length;
+        boolean[] visited =  new boolean[n];
+        int[] inrecursion = new int[n];
+        Arrays.fill(inrecursion,-1);
+        for(int i=0;i<n;i++){
+            if(!visited[i]) dfs(i,edges,visited, inrecursion,0);
+        }
+        return max;
+    }
+
+    private void dfs(int u , int[] edges ,boolean[] visited , int[] inrecursion,int count ){
+        if(visited[u] && inrecursion[u]!=-1){
+            max = Math.max(max,count-inrecursion[u]);
+            return;
+        }
+        if(visited[u]) return;
+        visited[u]=true;
+        inrecursion[u]=count;
+        count++;
+        if(edges[u]!=-1) dfs(edges[u],edges,visited,inrecursion,count);
+        inrecursion[u]=-1;
+    }
+}
+
+// where each node has at most one outgoing edge. 
+// -> this line tell us that we cannot have cycle merge with each other 
