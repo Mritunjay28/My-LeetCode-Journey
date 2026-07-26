606a8bdf1d99f2e5f86d6424cfb0df3e0762cbd8
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-07-26 09:29:26 +0530

    Time: 2 ms (99.61%), Space: 47.2 MB (93.89%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,33 +1,34 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        int n= nums.length;
-        if(n==3) return nums[0]*nums[1]*nums[2];
-        Arrays.sort(nums);
-        int smallest = nums[0];
-        int secondsmallest = nums[1];
-        int thirdsmallest = nums[2];
-        int largest = nums[n-1];
-        int secondlargest = nums[n-2];
-        int thirdlargest = nums[n-3];
 
-        // all neg then 3 largest values  
-        // all positive then 3 largest values
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
 
-        // if mix 1 neg then 3 largest 
-        // if 2 neg then check if smallest *secondsmallest > secondlargest*thirdlargest
-        // if 3 or more then also 2 neg then largest 
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
 
-        if(largest <=0 || smallest >=0){
-            return largest * secondlargest * thirdlargest;
+        for (int num : nums) {
+
+            if (num >= max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num >= max2) {
+                max3 = max2;
+                max2 = num;
+            } 
+            else if (num > max3) max3 = num;
+            
+
+            if (num <= min1) {
+                min2 = min1;
+                min1 = num;
+            } 
+            else if (num < min2) min2 = num;
+            
         }
 
-        if(secondsmallest >=0) return largest * secondlargest * thirdlargest;
-       
-            int negmax = smallest * secondsmallest;
-            int posmax = secondlargest * thirdlargest;
-
-            return largest* Math.max(negmax,posmax);
-        
-        
+        return Math.max(max1 * max2 * max3, max1 * min1 * min2); 
     }
 }
