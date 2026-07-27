9e7f1af6a69183661ec697a8d7c2b326ae024b3f
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-07-27 18:15:41 +0530

    Time: 111 ms (21.38%), Space: 273.4 MB (9.21%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,17 +1,19 @@
 class Solution {
     int[] dt; // discovery time 
     int[] low; // lowest discovery time across neighbour except parent
-    int time =0;
+    int time = 0;
     List<List<Integer>> ans;
+
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
         ans = new ArrayList<>();
         dt = new int[n];
         low = new int[n];
 
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
 
-        for(int i=0;i<connections.size();i++){
+        for (int i = 0; i < connections.size(); i++) {
             int u = connections.get(i).get(0);
             int v = connections.get(i).get(1);
 
@@ -21,33 +23,38 @@ class Solution {
 
         boolean[] visited = new boolean[n];
 
-        dfs(0,-1,adj,visited);
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, -1, adj, visited);
+            }
+        }
 
         return ans;
     }
 
-    public void dfs(int u , int parent ,List<List<Integer>> adj,boolean[] visited ){
+    public void dfs(int u, int parent, List<List<Integer>> adj, boolean[] visited) {
         visited[u] = true;
 
-        dt[u]=low[u]= ++time;
+        dt[u] = low[u] = ++time;
 
-        for(int v : adj.get(u)){
-            if(v==parent) continue;
+        for (int v : adj.get(u)) {
+            if (v == parent)
+                continue;
 
-            if(!visited[v]){
-                dfs(v,u,adj,visited);
+            if (!visited[v]) {
+                dfs(v, u, adj, visited);
 
-                low[u] = Math.min(low[u],low[v]);
+                low[u] = Math.min(low[u], low[v]);
 
-                if(low[v] > dt[u]){ // bridge exist
+                if (low[v] > dt[u]) { // bridge exist
                     ans.add(new ArrayList<>());
-                    ans.get(ans.size()-1).add(u);
-                    ans.get(ans.size()-1).add(v);
+                    ans.get(ans.size() - 1).add(u);
+                    ans.get(ans.size() - 1).add(v);
                 }
             }
 
-            else if (visited[v] && v!=parent) {
-                low[u] = Math.min(low[u],dt[v]);
+            else if (visited[v] && v != parent) {
+                low[u] = Math.min(low[u], dt[v]);
             }
         }
     }
