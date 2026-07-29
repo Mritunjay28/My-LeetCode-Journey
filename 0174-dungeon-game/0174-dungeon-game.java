b67ffa87cc43e3adf93eb47ec9efe361c44eca5c
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-07-29 17:53:43 +0530

    Time: 3 ms (28.04%), Space: 45.3 MB (56.2%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -6,10 +6,12 @@ class Solution {
     
     int[][] health = new int[m][n];
 
-    health[m - 1][n - 1] = Math.max(1 - dungeon[m - 1][n - 1], 1);
+    health[m - 1][n - 1] = Math.max(1 -dungeon[m - 1][n - 1] , 1); 
 
     for (int i = m - 2; i >= 0; i--) {            
         health[i][n - 1] = Math.max(health[i + 1][n - 1] - dungeon[i][n - 1], 1);
+        // if value is +ve and greater than need then it require only 1 as entry 
+        // else if -v1 thne requirement + below requirement
     }
 
     for (int j = n - 2; j >= 0; j--) {
