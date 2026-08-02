2ca0c47d210d55b8e0cb07fefd77be1bf8a6449b
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-08-02 16:33:32 +0530

    Time: 0 ms (100%), Space: 43.2 MB (46.71%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -1,50 +1,45 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
-
         List<String> res = new ArrayList<>();
-        int i = 0;
 
-        while (i < words.length) {
+        int i=0;
 
-            int j = i;
-            int len = 0;
+        while(i<words.length){
+            int j=i;
+            int len =0;
 
-            while (j < words.length && len + words[j].length() + (j - i) <= maxWidth) {
-                len += words[j].length();
+            while(j<words.length && len+ words[j].length() + (j-i) <=maxWidth){
+                len+= words[j].length();
                 j++;
             }
 
-            int gaps = j - i - 1;
+            int gaps = j-1-i;
             StringBuilder sb = new StringBuilder();
 
-            if (j == words.length || gaps == 0) {
-
-                for (int k = i; k < j; k++) {
+            if(j== words.length || gaps==0){
+                for(int k=i;k<j;k++) {
                     sb.append(words[k]);
-                    if (k != j - 1) sb.append(" ");
+                    if(k!=j-1) sb.append(" ");
                 }
 
-                while (sb.length() < maxWidth)
-                    sb.append(" ");
+                while(sb.length() < maxWidth) sb.append(" ");
+            }
+            else{
+                int space = maxWidth - len;
+                int even = space / gaps;
+                int extra = space % gaps;
 
-            } else {
-
-                int spaces = maxWidth - len;
-                int even = spaces / gaps;
-                int extra = spaces % gaps;
-
-                for (int k = i; k < j; k++) {
+                 for(int k=i;k<j;k++) {
                     sb.append(words[k]);
-
-                    if (k != j - 1) {
-                        for (int s = 0; s < even; s++) sb.append(" ");
-                        if (extra-- > 0) sb.append(" ");
-                    }
+                    if(k!=j-1){
+                        for(int l=0;l<even;l++) sb.append(" ");
+                        if(extra-- > 0) sb.append(" ");
+                    } 
                 }
             }
 
             res.add(sb.toString());
-            i = j;
+            i=j;
         }
 
         return res;
