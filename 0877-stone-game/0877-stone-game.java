7ead6ee76b6f5c4da6d4880695fea37e7c3d055a
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-08-02 09:03:56 +0530

    Time: 0 ms (100%), Space: 43.2 MB (35.84%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,19 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        Integer[][] memo = new Integer[n][n];
-      
-        return f(0,n-1,piles,memo) >0;
-    }
-
-    public int f(int i,int j,int[] piles, Integer[][] memo){
-        if(i==j) return piles[i];
-
-        if(memo[i][j]!=null) return memo[i][j];
-
-        int first = piles[i] - f(i+1,j,piles,memo);
-        int last = piles[j] - f(i,j-1,piles,memo);
-
-        return memo[i][j] = Math.max(first,last);
+       return true;
     }
 }
