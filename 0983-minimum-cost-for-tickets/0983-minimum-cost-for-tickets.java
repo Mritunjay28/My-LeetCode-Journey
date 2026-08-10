cb2ec5e1feb7f780be08c089ec19029346126e30
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-08-10 11:35:52 +0530

    Time: 16 ms (6.51%), Space: 46.6 MB (6.62%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,7 +1,7 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         Arrays.sort(days);
-  //       return f(0,0,days,costs);
+ //        return f(0,0,days,costs);
         int n = days.length;
         int[][] dp = new int[n + 1][days[n - 1] + 31];
 
@@ -12,8 +12,8 @@ class Solution {
                     passexist = dp[i+1][j];
                 }
                 int cost1 = costs[0] + dp[i+1][days[i]];
-                int cost7 = costs[1] + dp[i+1][days[i] + 7 - 1]; 
-                int cost30 = costs[2] +dp[i+1][days[i] + 30 - 1];  
+                int cost7 = costs[1] + dp[i+1][days[i]+6]; 
+                int cost30 = costs[2] +dp[i+1][days[i]+29];  
 
                 dp[i][j] =  Math.min(passexist, Math.min(cost1, Math.min(cost7, cost30)));
             }
