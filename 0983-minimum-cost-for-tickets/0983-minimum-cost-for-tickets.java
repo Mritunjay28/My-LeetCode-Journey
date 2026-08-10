842f2533f63bb4af8c5cacda1ece57325aa7c95d
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-08-10 11:34:16 +0530

    Time: 17 ms (6.51%), Space: 46.7 MB (6.09%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,24 +1,38 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
-        // length up to the last travel + 1 day is good enough (no need for 365)
-        int lastDay = days[days.length - 1];
-        // dp[i] means up to i-th day the minimum cost of the tickets
-        int[] dp = new int[lastDay + 1];
-        boolean[] isTravelDays = new boolean[lastDay + 1];
-        // mark the travel days
-        for (int day : days)
-            isTravelDays[day] = true;
+        Arrays.sort(days);
+  //       return f(0,0,days,costs);
+        int n = days.length;
+        int[][] dp = new int[n + 1][days[n - 1] + 31];
 
-        for (int i = 1; i <= lastDay; i++) {
-            if (!isTravelDays[i]) { // no need to buy ticket if it is not a travel day
-                dp[i] = dp[i - 1];
-                continue;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = days[n - 1]; j >= 0; j--) {
+                int passexist = (int) 1e9;
+                if (j >= days[i]) {
+                    passexist = dp[i+1][j];
+                }
+                int cost1 = costs[0] + dp[i+1][days[i]];
+                int cost7 = costs[1] + dp[i+1][days[i] + 7 - 1]; 
+                int cost30 = costs[2] +dp[i+1][days[i] + 30 - 1];  
+
+                dp[i][j] =  Math.min(passexist, Math.min(cost1, Math.min(cost7, cost30)));
             }
-            // select which type of ticket to buy
-            dp[i] = costs[0] + dp[i - 1]; // 1-day
-            dp[i] = Math.min(costs[1] + dp[Math.max(i - 7, 0)], dp[i]); // 7-day
-            dp[i] = Math.min(costs[2] + dp[Math.max(i - 30, 0)], dp[i]); // 30-day
         }
-        return dp[lastDay];
+
+        return dp[0][0];
     }
+
+    // public int f(int i,int day,int[] days, int[] costs){
+    //     if(i==days.length) return 0;
+
+    // int passexist = (int) 1e9;
+    // if(day>=days[i]){
+    //     passexist = f(i+1,day,days,costs);
+    // }
+    // int cost1= costs[0]+f(i+1,day+1,days,costs);
+    // int cost7= costs[1]+f(i+1,day+7,days,costs);
+    // int cost30= costs[2]+f(i+1,day+30,days,costs);
+
+    // return Math.min(passexist,Math.min(cost1,Math.min(cost7,cost30)));
+    // }
 }
