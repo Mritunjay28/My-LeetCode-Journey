4e976b974ef1950896d5a947f5a85adae30fe32d
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-08-20 17:17:19 +0530

    Time: 1 ms (98.08%), Space: 46.7 MB (42.79%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -5,15 +5,15 @@ class Solution {
         arr[0] = nums[0];
         arr[n - 1] = nums[1];
         int idx = 0,
-            revIdx = n - 1;
+            rev = n - 1;
         for (int i = 2; i < n; i++) {
-            if (arr[idx] > arr[revIdx]) {
+            if (arr[idx] > arr[rev]) {
                 arr[++idx] = nums[i];
             } else {
-                arr[--revIdx] = nums[i];
+                arr[--rev] = nums[i];
             }
         }
-        for (int l = revIdx, r = n - 1; l < r; l++, r--) {
+        for (int l = rev, r = n - 1; l < r; l++, r--) {
             int tmp = arr[l];
             arr[l] = arr[r];
             arr[r] = tmp;
