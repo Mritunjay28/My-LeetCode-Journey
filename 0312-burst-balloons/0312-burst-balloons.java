4605b612a7f2ee050ee2bf8cc03c8fc5e3983748
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-08-24 07:31:19 +0530

    Time: 37 ms (98.91%), Space: 44.5 MB (50.47%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n + 2];
+        arr[0] = 1;
+        arr[n + 1] = 1;
+        for (int i = 0; i < n; i++) {
+            arr[i + 1] = nums[i];
+        }
+        //return f(1, n, arr);
+
+        int[][] dp = new int[n + 2][n + 2];
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (i > j)
+                    dp[i][j] = 0;
+                else {
+                    int max = 0;
+                    for (int k = i; k <= j; k++) {
+                        int coins = dp[i][k-1] + dp[k+1][j] + arr[i - 1] * arr[k] * arr[j + 1];
+                        max = Math.max(max, coins);
+                    }
+
+                    dp[i][j] = max;
+                }
+            }
+        }
+
+        return dp[1][n];
+    }
+
+    public int f(int i, int j, int[] arr) {
+        if (i > j)
+            return 0;
+
+        int max = 0;
+        for (int k = i; k <= j; k++) {
+            int coins = f(i, k - 1, arr) + f(k + 1, j, arr) + arr[i - 1] * arr[k] * arr[j + 1];
+            max = Math.max(max, coins);
+        }
+
+        return max;
+    }
+}
