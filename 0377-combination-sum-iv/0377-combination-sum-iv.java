d3aa93f1c40c6bcdc8635cfeee77994328d90b1e
Mritunjay Senapati <mritunjaysenspati.dev@gmail.com>
2026-08-29 11:08:01 +0530

    Time: 1 ms (67.47%), Space: 42.7 MB (45.34%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,18 +1,30 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-         Arrays.sort(nums);
-        // dp[i] mean no of combination till i value
+        // return f(nums,target);
+        int n = nums.length;
         int[] dp = new int[target + 1];
         dp[0] = 1;
-
-        for (int i = 1; i <= target; i++) {
-            for (int num : nums) {
-                if (num > i) break;
-                dp[i] += dp[i - num];
+        for (int j = 1; j <= target; j++) {
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (j - nums[i] >= 0) count += dp[j-nums[i]];
             }
+            dp[j] = count;
         }
 
         return dp[target];
+    }
 
+    public int f(int[] nums, int target) {
+        if (target == 0)
+            return 1;
+
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (target - nums[i] >= 0)
+                count += f(nums, target - nums[i]);
+        }
+
+        return count;
     }
 }
